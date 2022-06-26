1de63639d87b3a1578fbca319f3fbed7be213814
Velidahia <100213768+Velidahia@users.noreply.github.com>
2022-06-26 08:31:54 -0700

    Add files via upload

---
+++ drf_simple/api/urls.py
@@ -0,0 +1,6 @@
+#api > urls.py
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.getData)
+]
---
+++ drf_simple/api/views.py
@@ -0,0 +1,11 @@
+#api > views.py
+from rest_framework.response
+import Response
+from
+rest_framework.decorators
+import api_view
+@api_view(['GET'])
+def getData(request):
+    person=
+    {'name':'Dennis','age':28}
+    return Response(person)
